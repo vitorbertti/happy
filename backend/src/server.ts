4e79aaa8146324ca8725e2cc35c1ddbb1b9a2481
Vitor Bertti <berttivitor@gmail.com>
2020-10-16 22:09:04 -0300

    Added orphanages controller

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,23 +1,9 @@
 import express from 'express';
-import { getRepository } from 'typeorm';
-import Orphanages from './models/Orphanages';
-
 import './database/connection';
+import routes from './routes';
 
 const app = express();
 app.use(express.json());
-
-app.post('/orphanages', async (req, res) => {
-   const { name, latitude, longitude, about, instructions, opening_hours, open_on_weekends } = req.body;
-
-   const orphanagesRepository = getRepository(Orphanages);
-   const orphanage = orphanagesRepository.create({
-      name, latitude, longitude, about, instructions, opening_hours, open_on_weekends
-   });
-
-   await orphanagesRepository.save(orphanage);
-   return res.json();
-});
-
+app.use(routes);
 
 app.listen(3333);
