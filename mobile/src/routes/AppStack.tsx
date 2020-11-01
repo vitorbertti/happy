6078fc33ebeaea11f3981f33645ea92ed6843bb7
Vitor Bertti <berttivitor@gmail.com>
2020-11-01 17:28:59 -0300

    Created OrphanageData component

--- mobile/src/routes/AppStack.tsx
+++ mobile/src/routes/AppStack.tsx
@@ -7,7 +7,7 @@ const { Navigator, Screen } = createStackNavigator();
 
 import OrphanagesMap from '../pages/OrphanagesMap';
 // import SelectMapPosition from '../pages/CreateOrphanage/SelectMapPosition';
-// import OrphanageData from '../pages/CreateOrphanage/OrphanageData';
+import OrphanageData from '../pages/CreateOrphanage/OrphanageData';
 // import VisitationData from '../pages/CreateOrphanage/VisitationData';
 import OrphanageDetails from '../pages/OrphanageDetails';
 import Header from '../components/Header';
@@ -34,14 +34,14 @@ export default function Routes() {
           }}
         /> */}
         
-        {/* <Screen 
+        <Screen 
           name="OrphanageData" 
           component={OrphanageData}
           options={{ 
             headerShown: true,
             header: (props) => <Header title="Dados do orfanato" {...props} />,
           }}
-        /> */}
+        />
         
         <Screen 
           name="OrphanageDetails"
