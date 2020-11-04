c5ee16e420da601e4185fb83cd3c0911555869e0
Vitor Bertti <berttivitor@gmail.com>
2020-11-04 19:42:41 -0300

    Added map position

--- mobile/src/pages/CreateOrphanage/SelectMapPosition.tsx
+++ mobile/src/pages/CreateOrphanage/SelectMapPosition.tsx
@@ -1,7 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { View, StyleSheet, Dimensions, Text } from 'react-native';
+
+import { useNavigation } from '@react-navigation/native';
+import { RectButton } from 'react-native-gesture-handler';
+import MapView, { MapEvent, Marker } from 'react-native-maps';
+
+import mapMarkerImg from '../../images/map-marker.png';
 
 export default function SelectMapPosition() {
+   const navigation = useNavigation();
+   const [position, setPosition] = useState({ latitude: 0, longitude: 0 });
+
+   function handleSelectMapPosition(event: MapEvent) {
+      setPosition(event.nativeEvent.coordinate);
+   }
+
+   function handleNextStep() {
+      navigation.navigate('OrphanageData', { position });
+   }
+
    return (
-      <h1>SelectMapPosition</h1>
+      <View style={styles.container}>
+         <MapView
+            initialRegion={{
+               latitude: -27.2092052,
+               longitude: -49.6401092,
+               latitudeDelta: 0.008,
+               longitudeDelta: 0.008,
+            }}
+            onPress={handleSelectMapPosition}
+            style={styles.mapStyle}
+         >
+            {!!position.latitude && (
+               <Marker
+                  icon={mapMarkerImg}
+                  coordinate={position}
+               />
+            )}
+         </MapView>
+
+         { !!position.latitude && (
+            <RectButton style={styles.nextButton} onPress={handleNextStep}>
+               <Text style={styles.nextButtonText}>Próximo</Text>
+            </RectButton>
+         )}
+      </View>
    )
 }
